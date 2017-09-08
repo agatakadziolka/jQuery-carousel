e741322f25770435ce407277fb536cff20034ce6
Agata <agatakad@gmail.com>
2017-09-08 22:32:18 +0200

    Poprawki slidera. Poprawnie przesuwa się w lewo. Źle w prawo

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@ $(function() {
 	var prev = $(".prev");
 	var next = $(".next");
 
-	setInterval(changeSlide, 3000);
+//	setInterval(changeSlide, 3000);
 	function changeSlide() {
 		carouselList.animate({'marginLeft': -500}, 500, moveFirstSlide);
 	};
@@ -23,17 +23,26 @@ $(function() {
 		carouselList.css({marginLeft:0});
 	};
 
-	function slideRight() {
-		carouselList.animate({'marginLeft': -500}, 500, moveFirstSlide);
+	function prevSlide() {
+		var firstItem = carouselList.find("li:first");
+		var lastItem = carouselList.find("li:last");
+		firstItem.before(lastItem);
+		carouselList.css({marginLeft: '-500'});
+		carouselList.animate({'marginLeft': '0'}, 500);
 	};
+
 	
-	function slideLeft() {
-		carouselList.animate({'marginLeft': +500}, 500, moveLastSlide);
+	function nextSlide() {
+		var firstItem = carouselList.find('li:first');
+		var lastItem = carouselList.find('li:last');
+		lastItem.after(firstItem);
+		carouselList.css({marginLeft: '0'});
+		carouselList.animate({'marginLeft': '-500'}, 500);
 	};
-	
-	next.on('click', slideRight);
-	prev.on('click', slideLeft);
-	
+
+
+	next.on('click', nextSlide);
+	prev.on('click', prevSlide);
 
 });
 
